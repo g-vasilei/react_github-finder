cc4ce0c7683ce0dff686172842cb1b830bed7766
g-vasilei <ram.kunya.9@gmail.com>
2022-03-06 09:59:15 +0200

    commit app to github

---
+++ src/pages/About.jsx
@@ -0,0 +1,10 @@
+function About() {
+   return (
+      <div className='px-2 mx-2'>
+         <h1 className='text-6xl mb-4'>Github Finder</h1>
+         <p className='text-lg'>This is a project that searchs and find github repos</p>
+      </div>
+   );
+}
+
+export default About;
